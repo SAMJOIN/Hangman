0471f1b6789be42ef061b3db61629c4f578cd075
SAMJOIN <nikita.korneev.2222@mail.ru>
2023-07-23 18:27:24 +0500

    commit with menu

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,5 +1,6 @@
 import styles from './../Styles/menu.module.css'
-import {selectCategorySportActionCreator,selectCategoryNatureActionCreator,selectCategoryTechActionCreator} from './../State/menu-reducer'
+import {selectCategorySportActionCreator,selectCategoryNatureActionCreator,selectCategoryTechActionCreator,selectCategoryChimestryActionCreator,
+    selectCategoryCountryActionCreator,selectCategoryWeaponActionCreator} from './../State/menu-reducer'
 
 function Menu(props) {
 
@@ -12,15 +13,27 @@ function Menu(props) {
     function selectCategoryNature() {
         props.dispatch(selectCategoryNatureActionCreator());
     }
+    function selectCategoryChimestry() {
+        props.dispatch(selectCategoryChimestryActionCreator());
+    }
+    function selectCategoryCountry() {
+        props.dispatch(selectCategoryCountryActionCreator());
+    }
+    function selectCategoryWeapon() {
+        props.dispatch(selectCategoryWeaponActionCreator());
+    }
 
     return (
         <div className={styles.menu}>
-            <h1>Тема</h1>
-            <p>Угадано слов: {props.state.menu.count}</p>
+            <h1 className={styles.themeLabel}>Тема</h1>
+            <p className={styles.countLabel}>Угадано слов: {props.state.menu.count}</p>
             <div className={styles.selector}>
                 <button className={styles.categoryBtn} onClick={selectCategorySport} >Спорт</button>
                 <button className={styles.categoryBtn} onClick={selectCategoryNature} >Природа</button>
                 <button className={styles.categoryBtn} onClick={selectCategoryTech} >Техника</button>
+                <button className={styles.categoryBtn} onClick={selectCategoryChimestry} >Химия</button>
+                <button className={styles.categoryBtn} onClick={selectCategoryCountry} >Страны</button>
+                <button className={styles.categoryBtn} onClick={selectCategoryWeapon} >Оружие</button>
             </div>
         </div>
     );
